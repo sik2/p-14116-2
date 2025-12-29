d616df5bd1fc3ad0b7d102d2d5b65c942d9cdc7d
jhs512 <jangka512@gmail.com>
2025-12-29 15:54:14 +0900

    0006

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.back.global.jpa.entity;
 
+import com.back.global.global.GlobalConfig;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
@@ -16,5 +17,9 @@ public abstract class BaseEntity {
 
     public String getModelTypeCode() {
         return this.getClass().getSimpleName();
+    }
+
+    protected void publishEvent(Object event) {
+        GlobalConfig.getEventPublisher().publish(event);
     }
 }
